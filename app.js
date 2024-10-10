9567ceeb446ed78c9327fcea8b972842d073e6af
Bryanhenrique1803 <bryan.pavanelli@escola.pr.gov.br>
2024-10-10 09:00:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+criarCartao(categoria, pergunta, resposta) {
+}
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta);
+    }
+    function criaCartao(categoria, pergunta, resposta) {
+
+        let container = document.getElementById(container);
+        let cartao = document.createElement(article)
+        cartao.className = cartao
+        }
+        cartao.innerHTML = 
---
+++ style.cs
@@ -0,0 +1,5 @@
+criaCartao(
+Língua Inglesa,
+Como se diz oi em Inglês?,
+Oi em inglês se diz HI (RAI)
+)
